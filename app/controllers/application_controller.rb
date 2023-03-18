1917e64ddb10be18dac4575c219304bfb5015e1d
Joshua Doud <bigjj3393@yahoo.com>
2023-03-18 12:50:38 -0700

    set up Get request for animals

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+  get '/animals' do 
+    animals = Animal.all 
+    animals.to_json 
+  end
 
 end
