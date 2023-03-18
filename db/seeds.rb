236b4d2ced812c6a9a3e0833ab1c7383a48d8a62
Joshua Doud <bigjj3393@yahoo.com>
2023-03-17 17:37:16 -0700

    Seeded animal data with faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,29 @@ Shelter.create(
 )
 
 puts "✅ Done seeding!"
+
+puts "🌱 Seeding animals..."
+
+10.times do 
+    Animal.create(
+        name: Faker::Creature::Dog.name,
+        species: "Dog",
+        breed: Faker::Creature::Dog.breed,
+        sex: Faker::Creature::Dog.gender,
+        age: rand(1..20),
+        shelter_id: rand(1..3)
+    )
+end
+
+10.times do 
+    Animal.create(
+        name: Faker::Creature::Cat.name,
+        species: "Cat",
+        breed: Faker::Creature::Cat.breed,
+        sex: Faker::Creature::Dog.gender,
+        age: rand(1..20),
+        shelter_id: rand(1..3)
+    )
+end
+
+puts "✅ Done seeding!"
