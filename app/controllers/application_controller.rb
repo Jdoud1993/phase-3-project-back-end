610295534299084d398a577712be6488337a993b
Joshua Doud <bigjj3393@yahoo.com>
2023-03-24 10:00:39 -0700

    created delete controller for animal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
+
+  # Animal Controllers Full CRUD
   get '/animals' do 
     animals = Animal.all 
     animals.to_json(include: :shelter) 
@@ -11,8 +13,14 @@ class ApplicationController < Sinatra::Base
     animal.to_json(include: :shelter)
   end
 
-  
+  delete '/animals/:id' do
+    animal = Animal.find(params[:id])
+    animal.destroy
+    animal.to_json
+  end
 
+
+  # Shelter Controllers READ/CREATE
   get '/shelters' do 
     shelters = Shelter.all 
     shelters.to_json 
